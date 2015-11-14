687c9b76413c8483a542eae5f1543777b16c65f1
azu <azuciao@gmail.com>
2015-11-14 12:39:29 +0900

    test(slice): add test pattern for slicing

--- test/parser-test.js
+++ test/parser-test.js
@@ -14,23 +14,52 @@ describe("parse", function () {
         var expected = '> <a name="test.js" href="fixtures/test.js">test.js</a>\n'
             + '\n'
             + '``` js\nconsole.log(\"test\");\n```';
-        console.log(expected);
         assert.equal(result.replaced, expected);
     });
-    it("should return sliced object for replace", function () {
-        var multiLineContent = "[include:4-6, line.js](fixtures/line.js)"
-        var results = parse(multiLineContent , __dirname);
-        assert(results.length > 0);
-        var result = results[0];
-        assert.equal(result.target, multiLineContent);
-        var expected = '> <a name="line.js" href="fixtures/line.js">line.js</a>\n'
-            + '\n'
-            + '``` js\n'
-            + 'console.log(\"this is line 4\");\n'
-            + 'console.log(\"this is line 5\");\n'
-            + 'console.log(\"this is line 6\");\n'
-            + '```';
-        assert.equal(result.replaced, expected);
+    context("sliced text", function () {
+        it("should return sliced object for replace", function () {
+            var multiLineContent = "[include:4-6, line.js](fixtures/line.js)";
+            var results = parse(multiLineContent , __dirname);
+            assert(results.length > 0);
+            var result = results[0];
+            assert.equal(result.target, multiLineContent);
+            var expected = '> <a name="line.js" href="fixtures/line.js">line.js</a>\n'
+                + '\n'
+                + '``` js\n'
+                + 'console.log(\"this is line 4\");\n'
+                + 'console.log(\"this is line 5\");\n'
+                + 'console.log(\"this is line 6\");\n'
+                + '```';
+            assert.equal(result.replaced, expected);
+        });
+        it("should return sliced `start`- text", function () {
+            var multiLineContent = "[include:9-, line.js](fixtures/line.js)";
+            var results = parse(multiLineContent , __dirname);
+            assert(results.length > 0);
+            var result = results[0];
+            assert.equal(result.target, multiLineContent);
+            var expected = '> <a name="line.js" href="fixtures/line.js">line.js</a>\n'
+                + '\n'
+                + '``` js\n'
+                + 'console.log(\"this is line 9\");\n'
+                + 'console.log(\"this is line 10\");\n'
+                + '```';
+            assert.equal(result.replaced, expected);
+        });
+        it("should return sliced -`end` text", function () {
+            var multiLineContent = "[include:-2, line.js](fixtures/line.js)";
+            var results = parse(multiLineContent , __dirname);
+            assert(results.length > 0);
+            var result = results[0];
+            assert.equal(result.target, multiLineContent);
+            var expected = '> <a name="line.js" href="fixtures/line.js">line.js</a>\n'
+                + '\n'
+                + '``` js\n'
+                + 'console.log(\"this is line 1\");\n'
+                + 'console.log(\"this is line 2\");\n'
+                + '```';
+            assert.equal(result.replaced, expected);
+        });
     });
     describe("containIncludeLabel", function () {
         it("support import", function () {
