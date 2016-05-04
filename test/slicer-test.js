da7afd4d3a0ab6df407881bd884e775bf03528d5
azu <azuciao@gmail.com>
2016-05-04 12:21:41 +0900

    refactor(slice): refactoring slice feature

---
+++ test/slicer-test.js
@@ -0,0 +1,98 @@
+// LICENSE : MIT
+"use strict";
+const assert = require("power-assert");
+const fs = require("fs");
+const path = require("path");
+import {getSliceRange,hasRangeCommand, sliceCode} from "../src/slicer";
+const lineFixture = fs.readFileSync(path.join(__dirname, "fixtures/line.js"), "utf-8");
+describe("slicer-test", function () {
+    describe("#hasRangeCommand", function () {
+        context("when range is defined", function () {
+            it("should return [undefined, undefined]", function () {
+                assert(hasRangeCommand(`import:0-0 ,`));
+                assert(hasRangeCommand(`import:1-2 ,`));
+                assert(hasRangeCommand(`import:2-3 ,`));
+                assert(hasRangeCommand(`import:2- ,`));
+                assert(hasRangeCommand(`import:-3 ,`));
+            });
+        });
+        context("when range is not defined", function () {
+            it("should return [undefined, undefined]", function () {
+                assert(!hasRangeCommand(`import:0`));
+                assert(!hasRangeCommand(`import:test`));
+                assert(!hasRangeCommand(`import`));
+                assert(!hasRangeCommand(`1-5 only`));
+                assert(!hasRangeCommand(`1-5`));
+            });
+        });
+    });
+    describe("#getSliceRange", function () {
+        it("should return [start,end]", function () {
+            const [start, end] = getSliceRange(`import:1-5 , label`);
+            assert.equal(start, 1);
+            assert.equal(end, 5);
+        });
+        context("when not defined range", function () {
+            it("should return [undefined, undefined]", function () {
+                const [start, end] = getSliceRange(`import:test`);
+                assert(start === undefined);
+                assert(end === undefined);
+            });
+        });
+        context("when `import:1-2, hello-world.js`", function () {
+            it("should return [1, 2]", function () {
+                const [start, end] = getSliceRange(`import:1-2, hello-world.js`);
+                assert(start === 1);
+                assert(end === 2);
+            });
+        });
+        context("when `import:2-3, hello-world.js`", function () {
+            it("should return [2, 3]", function () {
+                const [start, end] = getSliceRange(`import:2-3, hello-world.js`);
+                assert(start === 2);
+                assert(end === 3);
+            });
+        });
+        context("when `import:2-, hello-world.js`", function () {
+            it("should return [2, undefined]", function () {
+                const [start, end] = getSliceRange(`import:2-, hello-world.js`);
+                assert(start === 2);
+                assert(end === undefined);
+            });
+        });
+        context("when `import:-3, hello-world.js`", function () {
+            it("should return [2, undefined]", function () {
+                const [start, end] = getSliceRange(`import:-3, hello-world.js`);
+                assert(start === undefined);
+                assert(end === 3);
+            });
+        });
+    });
+    describe("#sliceCode", function () {
+        it("should return sliced object for replace", function () {
+            const [start, end] = getSliceRange("include:4-6, line.js");
+            const result = sliceCode(lineFixture, start, end);
+            assert(result.length > 0);
+            const expected = 'console.log("this is line 4");\n'
+                + 'console.log("this is line 5");\n'
+                + 'console.log("this is line 6");';
+            assert.equal(result, expected);
+        });
+        it("should return sliced `start`- text", function () {
+            const [start, end] = getSliceRange("include:9-, line.js");
+            const result = sliceCode(lineFixture, start, end);
+            assert(result.length > 0);
+            const expected = 'console.log("this is line 9");\n'
+                + 'console.log("this is line 10");';
+            assert.equal(result, expected);
+        });
+        it("should return sliced -`end` text", function () {
+            const [start, end] = getSliceRange("include:-2, line.js");
+            const result = sliceCode(lineFixture, start, end);
+            assert(result.length > 0);
+            const expected = 'console.log("this is line 1");\n'
+                + 'console.log("this is line 2");';
+            assert.equal(result, expected);
+        });
+    });
+});
