0e1096315e67b9aff539d307304ebb63015967b4
azu <azuciao@gmail.com>
2016-08-12 17:44:40 +0900

    feat(options): support `template` options

--- src/include-codeblock.js
+++ src/include-codeblock.js
@@ -4,9 +4,10 @@ var path = require('path');
 import {parse} from "./parser"
 module.exports = {
     hooks: {
-        "page:before": function (page) {
+        "page:before": function(page) {
+            var options = this.options.pluginsConfig["include-codeblock"];
             var pageDir = path.dirname(page.rawPath);
-            var results = parse(page.content, pageDir);
+            var results = parse(page.content, pageDir, options);
             results.forEach(result => {
                 var {target, replaced} = result;
                 page.content = page.content.replace(target, replaced);
