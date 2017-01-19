30bd1193ae0848c3c08f9e6853801455b834f1ad
Guillaume Dollé <dolle.guillaume@gmail.com>
2017-01-19 21:36:35 +0100

    Rename options to avoid confusion

--- src/include-codeblock.js
+++ src/include-codeblock.js
@@ -9,9 +9,9 @@ aceCheck();
 module.exports = {
     hooks: {
         "page:before": function(page) {
-            var options = this.options.pluginsConfig["include-codeblock"];
+            this.getPluginConfig(opts,"include-codeblock");
             var pageDir = path.dirname(page.rawPath);
-            var results = parse(page.content, pageDir, options);
+            var results = parse(page.content, pageDir, opts);
             results.forEach(result => {
                 var {target, replaced} = result;
                 page.content = page.content.replace(target, replaced);
