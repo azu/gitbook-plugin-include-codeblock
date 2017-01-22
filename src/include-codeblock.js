9aab0f5324717e000bb24815c3f5d546ff60ba9f
Guillaume Dollé <dolle.guillaume@gmail.com>
2017-01-22 16:14:18 +0100

    Revert options get

--- src/include-codeblock.js
+++ src/include-codeblock.js
@@ -9,9 +9,9 @@ aceCheck();
 module.exports = {
     hooks: {
         "page:before": function(page) {
-            this.getPluginConfig(opts,"include-codeblock");
+            var options = this.options.pluginsConfig["include-codeblock"];
             var pageDir = path.dirname(page.rawPath);
-            var results = parse(page.content, pageDir, opts);
+            var results = parse(page.content, pageDir, options);
             results.forEach(result => {
                 var {target, replaced} = result;
                 page.content = page.content.replace(target, replaced);
